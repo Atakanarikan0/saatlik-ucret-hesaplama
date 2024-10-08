bf3ef4f54f9d4ad43d1fd551634474fd2227f025
[Atakan Arıkan] <[atakanarkn1@outlook.com]>
2024-10-08 17:30:05 +0300

    toplam kazanç hesaplama

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let hour = Number(prompt('Günde kaç saat çalışıyorsunuz ?'));
+let earn = Number(prompt('Saat başı ücretiniz ne kadar ?'));
+
+let total = hour * earn;
+
+alert('Günlük kazancınız = ' + total);
